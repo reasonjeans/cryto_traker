d03aa23e225c808719e7a9a8dbb70ddef7574540
reasonjeans <yujin@yujinui-MacBookAir.local>
2023-01-27 16:59:26 +0900

    Chart Feat : add candelstick chart
    - add candelstick chart
    - fix price page

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -6,6 +6,14 @@ import { fetchCoinHistory } from '../api';
 import { useRecoilValue } from 'recoil';
 import { isDarkAtom } from '../atoms';
 
+import styled from 'styled-components';
+
+const ChartBox = styled.div`
+  p {
+    margin: 20px 0;
+  }
+`;
+
 interface IHistory {
   time_open: string;
   time_close: string;
@@ -36,44 +44,95 @@ function Chart({ coinId }: ChartProps) {
       {isLoading ? (
         'Loading chart...'
       ) : (
-        <ApexChart
-          type="line"
-          series={[
-            {
-              name: 'Price',
-              data: data?.map((price) => price.close) ?? [],
-            },
-          ]}
-          options={{
-            theme: { mode: isDark ? 'dark' : 'light' },
-            chart: {
-              width: 500,
-              height: 500,
-              toolbar: { show: false },
-            },
-            xaxis: {
-              axisBorder: { show: false },
-              axisTicks: { show: false },
-              labels: { show: false },
-              type: 'datetime',
-              categories: data?.map((price) => price.time_close),
-            },
-            yaxis: { show: false },
-            fill: {
-              type: 'gradient',
-              gradient: {
-                gradientToColors: ['#0be881'],
-                stops: [0, 100],
-              },
-            },
-            colors: ['#0fbcf9'],
-            tooltip: {
-              y: {
-                formatter: (value) => `$${value.toFixed(2)}`,
-              },
-            },
-          }}
-        />
+        <>
+          <ChartBox>
+            <p>Line Chart</p>
+            <ApexChart
+              type="line"
+              series={[
+                {
+                  name: 'Price',
+                  data: data?.map((price) => price.close) ?? [],
+                },
+              ]}
+              options={{
+                theme: { mode: isDark ? 'dark' : 'light' },
+                chart: {
+                  width: 500,
+                  height: 500,
+                  toolbar: { show: false },
+                },
+                xaxis: {
+                  axisBorder: { show: false },
+                  axisTicks: { show: false },
+                  labels: { show: false },
+                  type: 'datetime',
+                  categories: data?.map((price) => price.time_close),
+                },
+                yaxis: { show: false },
+                fill: {
+                  type: 'gradient',
+                  gradient: {
+                    gradientToColors: ['#0be881'],
+                    stops: [0, 100],
+                  },
+                },
+                colors: ['#0fbcf9'],
+                tooltip: {
+                  y: {
+                    formatter: (value) => `$${value.toFixed(2)}`,
+                  },
+                },
+              }}
+            />
+          </ChartBox>
+          <ChartBox>
+            <p>Candle Chart</p>
+            <ApexChart
+              type="candlestick"
+              series={[
+                {
+                  name: 'Price',
+                  data:
+                    data?.map((price) => ({
+                      x: price.time_close,
+                      y: [price.open, price.high, price.low, price.close],
+                    })) ?? [],
+                },
+              ]}
+              options={{
+                theme: { mode: isDark ? 'dark' : 'light' },
+                chart: {
+                  width: 500,
+                  height: 500,
+                  toolbar: { show: false },
+                },
+                grid: { show: false },
+                xaxis: {
+                  axisBorder: { show: false },
+                  axisTicks: { show: false },
+                  labels: { show: false },
+                  type: 'datetime',
+                  categories: data?.map((price) => price.time_close),
+                },
+                yaxis: { show: false },
+                plotOptions: {
+                  candlestick: {
+                    colors: {
+                      upward: '#f00',
+                      downward: '#00f',
+                    },
+                  },
+                },
+                tooltip: {
+                  y: {
+                    formatter: (value) => `$${value.toFixed(2)}`,
+                  },
+                },
+              }}
+            />
+          </ChartBox>
+        </>
       )}
     </div>
   );
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -292,7 +292,7 @@ function Coin() {
 
           <Switch>
             <Route path={`/:coinId/price`}>
-              <Price coinId={coinId} />
+              <Price coinId={coinId} tikersData={tikersData} />
             </Route>
             <Route path={`/:coinId/chart`}>
               <Chart coinId={coinId} />
